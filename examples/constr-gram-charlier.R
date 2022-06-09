72645a00b151d153b3ea8451fa697fb6d772b26d
wilsonfreitas <wilson.freitas@gmail.com>
2022-06-09 16:37:38 -0300

    Updated example

--- examples/constr-gram-charlier.R
+++ examples/constr-gram-charlier.R
@@ -53,7 +53,7 @@ ch <- cotahist_get(refdate, "daily")
 yc <- yc_get(refdate)
 op <- cotahist_equity_options_superset(ch, yc)
 
-symbol_ <- "ABEV3"
+symbol_ <- "VALE3"
 op1 <- op |>
   filter(
     symbol.underlying == symbol_
@@ -63,7 +63,7 @@ maturities <- unique(op1$maturity_date) |> sort()
 close_underlying <- op1$close.underlying[1]
 
 op_vol <- op1 |>
-  filter(maturity_date %in% maturities[1]) |>
+  filter(maturity_date %in% maturities[2]) |>
   mutate(
     biz_days = bizdays(
       refdate, following(maturity_date, "Brazil/B3"), "Brazil/B3"
@@ -86,7 +86,7 @@ op_vol <- op1 |>
 
 # ----
 
-grad <- function(par, type, spot, strike, rate, time, y.data, sy.data) {
+grad_obj_csm <- function(par, type, spot, strike, rate, time, y.data, sy.data) {
   par_ <- uncons_regionD(par[2], par[3])
   par <- c(par[1], par_[1], par_[2])
 
@@ -111,28 +111,55 @@ grad <- function(par, type, spot, strike, rate, time, y.data, sy.data) {
   ret
 }
 
-f.optim.csm <- function(par, type, spot, strike, rate, time, y.data, sy.data) {
+grad2_obj_csm <- function(par,
+                          type, spot, strike, rate, time, y.data, sy.data) {
+  bump <- 1e-4
+
+  p <- c(par[1] + bump, par[2], par[3])
+  du <- f_obj_csm(p, type, spot, strike, rate, time, y.data, sy.data)
+  p <- c(par[1] - bump, par[2], par[3])
+  dd <- f_obj_csm(p, type, spot, strike, rate, time, y.data, sy.data)
+  grad1 <- (du - dd) / (2 * bump)
+
+  p <- c(par[1], par[2] + bump, par[3])
+  du <- f_obj_csm(p, type, spot, strike, rate, time, y.data, sy.data)
+  p <- c(par[1], par[2] - bump, par[3])
+  dd <- f_obj_csm(p, type, spot, strike, rate, time, y.data, sy.data)
+  grad2 <- (du - dd) / (2 * bump)
+
+  p <- c(par[1], par[2], par[3] + bump)
+  du <- f_obj_csm(p, type, spot, strike, rate, time, y.data, sy.data)
+  p <- c(par[1], par[2], par[3] - bump)
+  dd <- f_obj_csm(p, type, spot, strike, rate, time, y.data, sy.data)
+  grad3 <- (du - dd) / (2 * bump)
+
+  c(grad1, grad2, grad3)
+}
+
+f_obj_csm <- function(par, type, spot, strike, rate, time, y.data, sy.data) {
   sigma <- par[1]
   params <- uncons_regionD(par[2], par[3])
-  yf <- csmprice(type, spot, strike, time, rate, 0, sigma, params[1], params[2])
+  par_ <- c(par[1], params)
+  yf <- csmprice(type, spot, strike, time, rate, 0, par_[1], par_[2], par_[3])
   ret <- sum(((yf - y.data) / sy.data)^2)
   ret
 }
 
 # ----
-typo <- "Call"
-res <- with(op_vol |> filter(type == typo), {
+
+typo <- c("Call", "Put")
+res <- with(op_vol |> filter(type %in% typo, !is.na(adj_delta)), {
   optim(
-    par = c(0.4, 0, 4), fn = f.optim.csm, gr = grad,
-    type, close.underlying, strike, rate, time_to_maturity, close, 1 / volume,
-    lower = c(1e-3, -Inf, -Inf), upper = Inf, method = "L-BFGS-B"
+    par = c(0.1, 0, 3), fn = f_obj_csm, gr = grad2_obj_csm,
+    method = "L-BFGS-B",
+    type, close.underlying, strike, rate, time_to_maturity, close, 1
   )
 })
 
 params <- uncons_regionD(res$par[2], res$par[3])
 
 op_vol_f <- op_vol |>
-  filter(type == typo) |>
+  filter(type %in% typo) |>
   mutate(
     theo_price = csmprice(
       type, close.underlying, strike, time_to_maturity, rate, 0,
@@ -144,6 +171,11 @@ op_vol_f <- op_vol |>
   )
 
 op_vol_f |>
+  mutate(error = (close - theo_price) / close) |>
+  ggplot(aes(x = strike, y = error, size = volume, colour = type)) +
+  geom_point(alpha = 0.5)
+
+op_vol_f |>
   ggplot(aes(x = strike, y = bsm_impvol, colour = type)) +
   geom_point(alpha = 0.5) +
   geom_line(aes(y = csm_impvol), colour = "black") +
@@ -151,6 +183,44 @@ op_vol_f |>
   theme(legend.position = "none")
 
 op_vol_f |>
-  mutate(error = close - theo_price) |>
-  ggplot(aes(x = strike, y = error, size = volume)) +
-  geom_point(alpha = 0.5)
+  ggplot(aes(x = strike, y = close, colour = type)) +
+  geom_point(alpha = 0.5) +
+  geom_line(aes(y = theo_price), colour = "black") +
+  theme(legend.position = "none") +
+  facet_wrap(. ~ type)
+
+# ----
+
+obj_space <- function(df, typo) {
+  function(par) {
+    par <- c(0.40, par)
+    with(df |> filter(type %in% typo, !is.na(adj_delta)), {
+      f_obj_csm(
+        par, type, close.underlying, strike, rate, time_to_maturity, close, 1
+      )
+    })
+  }
+}
+
+ff <- obj_space(op_vol, "Call")
+
+comb <- expand.grid(
+  mu3 = seq(-5, 5, length.out = 50),
+  mu4 = seq(-5, 5, length.out = 50)
+)
+
+z <- sapply(seq_len(dim(comb)[1]), function(i) {
+  parms <- as.numeric(comb[i, ])
+  ff(parms)
+})
+
+comb$ll <- z
+h <- hist(z, 10, plot = FALSE)
+comb$llgroups <- cut(z, h$breaks)
+
+comb[which.min(z), ]
+
+theme_set(theme_bw(base_size = 9))
+ggplot(comb, aes(x = mu4, y = mu3, fill = z)) +
+  geom_tile() +
+  labs(x = "kurtosis", y = "skewness", fill = "llh")
